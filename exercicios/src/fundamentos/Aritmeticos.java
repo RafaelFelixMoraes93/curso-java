fbc767aff1237188a67e96d51b273e66e090c72b
Rafael Felix <rafael.felix.moraes@gmail.com>
2024-06-14 16:41:20 -0300

    Aritméticos

---
+++ exercicios/src/fundamentos/Aritmeticos.java
@@ -0,0 +1,29 @@
+package fundamentos;
+
+public class Aritmeticos {
+	public static void main(String[] args) {
+		
+		System.out.println(2 + 3);
+		var valorX = 34.56;
+		double valorY = 2.2;
+		
+		System.out.println(valorX + valorY);
+		
+		int variavelA = 8;
+		int varivaelB = 3;
+		
+		System.out.println(variavelA + varivaelB);
+		System.out.println(variavelA - varivaelB);
+		System.out.println(variavelA * variavelA);
+		System.out.println(variavelA / varivaelB);
+		System.out.println(variavelA / (double) varivaelB);
+		System.out.println(variavelA / (float) varivaelB);
+		
+		System.out.println(8 % 3);
+		System.out.println(variavelA % varivaelB);	
+		
+		System.out.println(valorY + valorY - variavelA * varivaelB);
+		
+	}
+
+}
