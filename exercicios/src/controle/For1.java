29005fba39d92bd9be37b4b2f6b9ba360f7b5538
Rafael Felix <rafael.felix.moraes@gmail.com>
2024-07-04 21:35:35 -0300

    feat: For

---
+++ exercicios/src/controle/For1.java
@@ -0,0 +1,26 @@
+package controle;
+
+public class For1 {
+	public static void main(String[] args) {
+		
+		
+		for(int contando = 0; contando <= 10; contando++) {
+			System.out.println("Bom dia! " + contando);
+		}
+		
+		/*int x = 2;
+		*for(; x <=10;) {
+		*	System.out.println("x " + x);
+		*	x++;
+		*}
+		* Embora seja possível, não é uma boa prática de programação.
+		*/
+		
+		/*for(;true;) {
+		*	System.out.println("Fim!");
+		*}
+		*Laço infinito.
+		*/
+	}
+
+}
